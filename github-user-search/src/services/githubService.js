82f3c1e6d7d21082c2e87f624af51df33d753ab6
ufuoma blessed <ufuomablessed17@gmail.com>
2025-09-20 21:35:46 +0100

    add search to identify github users

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com/users";
 
-export const fetchUser = async (username) => {
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}/${username}`);
     return response.data;
   } catch (error) {
-    console.error("Error fetching user:", error);
     throw error;
   }
 };
